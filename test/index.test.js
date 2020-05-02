c186e987272091c411553ceb81db730f2e9ba45c
Hrishikesh Bawane <hrishibawane.hvb@gmail.com>
2020-05-02 18:00:49 +0530

    Update add and subtract functions
    Add and subtract functions can handle multi-dimensional arrays

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,29 @@
 const assert = require("assert");
 const np = require("../index");
 
-let a = [1, 2, 3, 4];
-let b = [1, 2, 3, 4];
+let a = [[1, 2, 3, 4]];
+let b = [[1, 2, 3, 4]];
 
 describe("Add arrays", () => {
   it("should add array elements", () => {
-    let c = [2, 4, 6, 8];
+    let c = [[2, 4, 6, 8]];
     let d;
     np.add(a, b, (err, res) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        d = res;
+      }
+    });
+    assert(c.toString() == d.toString());
+  });
+});
+
+describe("Subtract arrays", () => {
+  it("should subtract array elements", () => {
+    let c = [[0, 0, 0, 0]];
+    let d;
+    np.subtract(a, b, (err, res) => {
       if (err) {
         console.log(err.message);
       } else {
